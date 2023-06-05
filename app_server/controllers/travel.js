afd8dfb9d83de9065b8e30fc23d012787d324bca
jfloc <120976649+jfloc@users.noreply.github.com>
2023-06-04 21:19:48 -0500

    Express website, JSON

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,6 +1,6 @@
-var fs = require('fs');
+const fs = require('fs');
 
-var trips = JSON.parse(fs.readFileSync('./data/trips.json','utf8'));
+const trips = JSON.parse(fs.readFileSync('./data/trips.json','utf8'));
 
 const travel = (req, res) => {
     pageTitle = process.env.npm_package_description + ' - Travel';
